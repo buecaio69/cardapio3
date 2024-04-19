d062e5f1e2a2258179f5a0e2ee724ebc7e55f968
buecaio69 <buecaio2004@gmail.com>
2024-04-19 11:46:05 -0300

    menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'sans':['Roboto', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
-        "home": "url('./assets/logo hb.png')"
-      }
+        "home": "url(https://images2.imgbox.com/ce/5b/ToJzmFeW_o.png)"
+      },
+      filter: {
+        'brightness-70': 'brightness(0.7)',
+      },
     },
   },
   plugins: [],
